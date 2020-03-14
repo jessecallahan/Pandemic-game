8de192a674987c76cd13f304af06e297ab441c31
Jesse Callahan <jessetylercallahan@gmail.com>
2020-03-13 19:27:17 -0700

    adds timer/counter thing, doesnt get startInfection going or doesnt get a player construct built

--- src/main.js
+++ src/main.js
@@ -4,11 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
 import { Player } from './pandemic.js';
 
+
+player1 = {
+  roll: 0,
+  grandTotal: 0
+};
+
 $(document).ready(function () {
+
+  var sec = 0;
+  function pad(val) { return val > 9 ? val : "0" + val; }
+  setInterval(function () {
+    $("#seconds").html(pad(++sec % 365) + "days");
+    $("#minutes").html(pad(parseInt(sec / 365, 10)));
+  }, 1000);
+
   $('#pandemic-form').submit(function (event) {
     event.preventDefault();
-    let player1 = new Player()
-    console.log(player1.infectionLevel)
-    $('#solution').text(player1.infectionLevel);
+    newPlayer = new Player();
+    newPlayer.startInfection();
+    console.log(this.name)
+    console.log(newPlayer.infectionLevel)
+    $('#solution').text("test");
   });
-});
+
